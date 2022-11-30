d3c0f34c6e6003a54c50513e0ef233d877978c59
TheRealNock <92985337+TheRealNock@users.noreply.github.com>
2022-11-30 12:23:12 +0100

    :sparkles: CI Update #5

--- src/Services/AdvertentieService.js
+++ src/Services/AdvertentieService.js
@@ -6,16 +6,11 @@ class AdvertentieService{
 
     getAdvertenties(){
         return axios.get(ADVERTENTIE_API_BASE_URL);
-    }
-    createAdd(advertentie){
-        return axios.post(ADVERTENTIE_API_BASE_URL, advertentie);
-    }
-    getAdvertentieById(advertentieId){
-        return axios.get("http://localhost:8081/api/v1/advertentie/" + advertentieId)
+
     }
 
-    updateAdvertentie(advertentie, advertentieId){
-        return axios.put(ADVERTENTIE_API_BASE_URL + '/' + advertentieId, advertentie);
+    getAdvertentieById(advertentieId){
+        return axios.get("http://localhost:8081/api/v1/advertentie/" + advertentieId)
     }
 
     deleteAdvertentie(advertentieId){
